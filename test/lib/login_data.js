c4d6d99e40c78258c95d74dba126d377b97fde1e
Christopher Rankin <rankin.devon@gmail.com>
2014-06-13 23:37:53 -0500

    Try to source the .bashrc on travis-ci

--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -1,7 +1,7 @@
 //----- Data for login function -----
 exports.data = {
   webaddress: 'https://localhost:8443',
-  username: 'admin', //------- Enter the xTuple username
+  username: 'admin', //------- Enter the xCore username
   pwd: 'admin', //------ enter the password here
   org: 'dev' //------ enter the database name here
 };
