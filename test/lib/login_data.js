0949ecd778e357bbb1837149f3164a8ac47d522b
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 18:17:59 -0500

    Chall is my witness. This works on local.

--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -1,6 +1,9 @@
-exports.data = {
-	webaddress: 'https://localhost:8443',
-	username: 'admin', //------- Enter the xTuple username
-	pwd: 'admin', //------ enter the password here
-	org: 'dev' //------ enter the database name here
-};
+(function () {
+	module.exports = {
+		webaddress: 'https://localhost:8443',
+		username: 'admin', //------- Enter the xTuple username
+		pwd: 'admin', //------ enter the password here
+		org: 'dev' //------ enter the database name here
+	};
+}());
+
