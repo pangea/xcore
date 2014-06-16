7b882e6514f8abafd3229cd0114618708670e122
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 17:00:47 -0500

    change exporting syntax

--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -2,11 +2,13 @@
 	use "strict";
 
 	//----- Data for login function -----
-	exports.data = {
-  	webaddress: 'https://localhost:8443',
-  	username: 'admin', //------- Enter the xTuple username
-  	pwd: 'admin', //------ enter the password here
-  	org: 'dev' //------ enter the database name here
+	module.exports = {
+		data: {
+			webaddress: 'https://localhost:8443',
+			username: 'admin', //------- Enter the xTuple username
+			pwd: 'admin', //------ enter the password here
+			org: 'dev' //------ enter the database name here
+		}
 	};
 
 }());
