a002f7d56491e2884e6a1efdb94360393d7358af
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 18:09:49 -0500

    Going back to the original setup

--- test/build/build_app.js
+++ test/build/build_app.js
@@ -7,25 +7,22 @@ var buildAll 	= require('../../scripts/lib/build_all'),
   assert 			= require('chai').assert,
   datasource 	= require('../../node-datasource/lib/ext/datasource').dataSource,
   path 				= require('path');
-	loginData		= require(path.join(__dirname, "../lib/login_data.js")),
-  config					= require(path.join(__dirname, "../../node-datasource/config.js")),
 
 (function () {
   "use strict";
 
   describe('The database build tool', function () {
     this.timeout(100 * 60 * 1000);
-			//var datasource	= require('../../../xcore/node-datasource/lib/ext/datasource').dataSource,
-		
-
-      creds						= config.databaseServer;
-      //databaseName = loginData.org;
-		console.log("Config: " + JSON.stringify(config));	
-		console.log("creds: " + JSON.stringify(creds));
-		console.log("loginData: " + JSON.stringify(loginData));
-		console.log("DataSource: " + JSON.stringify(datasource));
-		/*
-    it('should build without error on a brand-new database', function (done) {
+    var loginData = require(path.join(__dirname, '../lib/login_data.js')).data,
+			datasource = require('../../../xcore/node-datasource/lib/ext/datasource').dataSource,
+      config = require(path.join(__dirname, "../../node-datasource/config.js")),
+      creds = config.databaseServer;
+		databaseName = loginData.org;
+		//console.log("Config: " + JSON.stringify(config));	
+		//console.log("creds: " + JSON.stringify(creds));
+		//console.log("loginData: " + JSON.stringify(loginData));
+    
+		it('should build without error on a brand-new database', function (done) {
       buildAll.build({
         database: databaseName,
         initialize: true,
@@ -35,8 +32,6 @@ var buildAll 	= require('../../scripts/lib/build_all'),
         done();
       });
     });
-		*/	
-		/*
     it('should grant all privileges to the user', function (done) {
       var sql = "insert into usrpriv (usrpriv_username, usrpriv_priv_id) " +
         "select $1, priv_id " +
@@ -51,7 +46,6 @@ var buildAll 	= require('../../scripts/lib/build_all'),
         done();
       });
     });
-		*/
   });
 }());
 
--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -1,11 +1,6 @@
-(function () {
-	"use strict";
-
-	module.exports = {
-		webaddress: 'https://localhost:8443',
-		username: 'admin', //------- Enter the xTuple username
-		pwd: 'admin', //------ enter the password here
-		org: 'dev' //------ enter the database name here
-	};
-
-}());
+exports.data = {
+	webaddress: 'https://localhost:8443',
+	username: 'admin', //------- Enter the xTuple username
+	pwd: 'admin', //------ enter the password here
+	org: 'dev' //------ enter the database name here
+};
