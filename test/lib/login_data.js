bc5c2cf68e48b34725a95d6a26e721141253c56c
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 17:21:23 -0500

    rever to old syntax

--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -1,15 +1,6 @@
-(function() {
-	use "strict";
-
-	//----- Data for login function -----
-	module.exports = {
-		data: {
-			webaddress: 'https://localhost:8443',
-			username: 'admin', //------- Enter the xTuple username
-			pwd: 'admin', //------ enter the password here
-			org: 'dev' //------ enter the database name here
-		}
-	};
-
-}());
-
+exports.data = {
+	webaddress: 'https://localhost:8443',
+	username: 'admin', //------- Enter the xTuple username
+	pwd: 'admin', //------ enter the password here
+	org: 'dev' //------ enter the database name here
+}
