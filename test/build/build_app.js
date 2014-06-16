df711e5cea2e54241f92a7da28aec9e6a850fbd0
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 15:54:28 -0500

    Try loading file directly

--- test/build/build_app.js
+++ test/build/build_app.js
@@ -13,13 +13,14 @@ var buildAll 	= require('../../scripts/lib/build_all'),
 
   describe('The database build tool', function () {
     this.timeout(100 * 60 * 1000);
-    var loginData = require(path.join(__dirname, "../lib/login_data.js")).data,
+    var loginData = require("../lib/login_data.js").data,
 			datasource = require('../../../xcore/node-datasource/lib/ext/datasource').dataSource,
       config = require(path.join(__dirname, "../../node-datasource/config.js")),
       creds = config.databaseServer;
       //databaseName = loginData.org;
 		console.log("Config: " + config);	
 		console.log("creds: " + creds);
+		console.log("loginData: " + creds);
 		/*
     it('should build without error on a brand-new database', function (done) {
       buildAll.build({
