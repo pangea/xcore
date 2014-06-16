4027d6c5155a037c5cfbacec38f0dd3a960ec361
Christopher Rankin <rankin.devon@gmail.com>
2014-06-16 17:38:11 -0500

    Put space between function and parenthesis

--- test/lib/login_data.js
+++ test/lib/login_data.js
@@ -1,8 +1,11 @@
-(function() {
+(function () {
+	"use strict";
+
 	module.exports = {
 		webaddress: 'https://localhost:8443',
 		username: 'admin', //------- Enter the xTuple username
 		pwd: 'admin', //------ enter the password here
 		org: 'dev' //------ enter the database name here
 	};
+
 }());
