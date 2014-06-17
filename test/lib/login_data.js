5f6ce98ae9209ef34341744f75699fbe2984ce11
Noah <noahprince8@gmail.com>
2014-06-17 12:29:33 -0500

    set db in .travis

--- test/lib/login_data.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	module.exports = {
-		webaddress: 'https://localhost:8443',
-		username: 'admin', //------- Enter the xTuple username
-		pwd: 'admin', //------ enter the password here
-		org: 'dev' //------ enter the database name here
-	};
-}());
-
