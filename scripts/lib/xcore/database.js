716b5a68a02ee4d6be348b45642b8f88757d053e
Mark McCanna <markmccanna1@gmail.com>
2014-07-08 13:11:43 -0500

    fixed

--- scripts/lib/xcore/database.js
+++ scripts/lib/xcore/database.js
@@ -200,48 +200,6 @@ logger				= require('./logger').logger;
 		* already exist in the database.
 	*/
 	var getExistingOrms = function(opts, creds) {
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
 		return function (callback) {
 			var existsSql = "select relname from pg_class where relname = 'orm'",
     	    ormTestSql = "select orm_namespace as namespace, " +
